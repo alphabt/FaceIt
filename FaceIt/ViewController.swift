9f0a36d1ea2288dcfdc6f61b4b611e26af2e8122
Brian Tai <alphabt@gmail.com>
2016-12-13 15:42:41 +0800

    Init FaceIt

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Brian Tai on 12/13/16.
+//  Copyright © 2016 Brian Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
